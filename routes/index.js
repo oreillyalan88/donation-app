26fd4d59673fc885f25411a6f0437b04067392ec
Alan O' Reilly <oreillyalan88@gmail.com>
2017-09-21 20:09:50 +0100

    customised index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Donation Web App' });
 });
 
 module.exports = router;
